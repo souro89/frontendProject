d2462129b08314034f5fc7cc5178aba6b21fcabb
souro89 <souro89@gmail.com>
2019-06-28 16:22:15 +0400

    index page completed

--- js/header.js
+++ js/header.js
@@ -1,15 +1,18 @@
 // Get the modal
 var signupmodal = document.getElementById("signupModal");
 var signinmodal = document.getElementById("signinModal");
+var createmodal = document.getElementById("createpost");
 
 // Get the button that opens the modal
 var btn = document.getElementById("signup");
 var signinbtn = document.getElementById("signin");
 var lsignup = document.getElementById("lingsignup");
+var createmodalbutton = document.getElementById("create-post");
 
 // Get the <span> element that closes the modal
 var span = document.getElementById("signupclose");
 var spansignin = document.getElementById("signinclose");
+var createclose = document.getElementById("createclose");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -25,6 +28,10 @@ lsignup.onclick = function() {
   signupmodal.style.display = "flex";
 };
 
+createmodalbutton.onclick = function() {
+  createmodal.style.display = "flex";
+};
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   signupmodal.style.display = "none";
@@ -32,6 +39,10 @@ span.onclick = function() {
 
 spansignin.onclick = function() {
   signinmodal.style.display = "none";
+};
+
+createclose.onclick = function() {
+  createmodal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
@@ -44,5 +55,11 @@ window.onclick = function(event) {
 window.onclick = function(event) {
   if (event.target == signinmodal) {
     signinmodal.style.display = "none";
+  }
+};
+
+window.onclick = function(event) {
+  if (event.target == createmodal) {
+    createmodal.style.display = "none";
   }
 };
